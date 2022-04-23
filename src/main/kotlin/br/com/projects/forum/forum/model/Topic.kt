1483807732bdc5fbca7318a913597f19f8469281
Joao <joaopedrolombardiv@gmail.com>
2022-04-23 20:23:01 -0300

    Refact

--- src/main/kotlin/br/com/projects/forum/forum/model/Topic.kt
+++ src/main/kotlin/br/com/projects/forum/forum/model/Topic.kt
@@ -3,12 +3,12 @@ package br.com.projects.forum.forum.model
 import java.time.LocalDateTime
 
 data class Topic (
-    val id: Long? = null,
+    var id: Long? = null,
     val title: String,
     val message: String,
-    val criationDate: LocalDateTime = LocalDateTime.now(),
+    val creationDate: LocalDateTime = LocalDateTime.now(),
     val course: Course,
     val author: User,
-    val status: TopicStatus = TopicStatus.NOT_RESOLVED,
+    val status: TopicStatus = TopicStatus.NOT_ANSWERED,
     val responses: List<Resposta> = ArrayList()
 )
