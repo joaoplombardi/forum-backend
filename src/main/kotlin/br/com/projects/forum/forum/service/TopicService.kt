1832fbd858050c2f1bb0695ce94886b4e08f7099
Joao <joaopedrolombardiv@gmail.com>
2022-04-29 21:46:26 -0300

    Refact(TopicService.kt): Utilizando mapper para a a transormação de dados

--- src/main/kotlin/br/com/projects/forum/forum/service/TopicService.kt
+++ src/main/kotlin/br/com/projects/forum/forum/service/TopicService.kt
@@ -4,7 +4,7 @@ import br.com.projects.forum.forum.dto.NewTopicForm
 import br.com.projects.forum.forum.dto.TopicDTO
 import br.com.projects.forum.forum.dto.UpdateTopicForm
 import br.com.projects.forum.forum.mapper.TopicFormMapper
-import br.com.projects.forum.forum.mapper.TopicoDtoMapper
+import br.com.projects.forum.forum.mapper.TopicDtoMapper
 import br.com.projects.forum.forum.model.Topic
 import org.springframework.stereotype.Service
 import java.util.stream.Collectors
@@ -12,28 +12,29 @@ import java.util.stream.Collectors
 @Service
 class TopicService(
     private var topics: List<Topic> = ArrayList(),
-    private val topicoDtoMapper: TopicoDtoMapper,
+    private val topicDtoMapper: TopicDtoMapper,
     private val topicFormMapper: TopicFormMapper
     ) {
 
     fun list(): List<TopicDTO> =
-        topics.stream().map { topicoDtoMapper.map(it) }.collect(Collectors.toList())
+        topics.stream().map { topicDtoMapper.map(it) }.collect(Collectors.toList())
 
     fun findById(id: Long): TopicDTO {
-        return topicoDtoMapper.map(
+        return topicDtoMapper.map(
             topics.stream().filter { it.id == id }.findFirst().get()
         )
     }
 
-    fun register(form: NewTopicForm) {
+    fun register(form: NewTopicForm): TopicDTO {
         val topic = topicFormMapper.map(form)
         topic.id = topics.size.toLong() + 1
         topics = topics.plus(topic)
+        return topicDtoMapper.map(topic)
     }
 
-    fun update(form: UpdateTopicForm){
+    fun update(form: UpdateTopicForm): TopicDTO{
         val topic = topics.stream().filter { it.id == form.id }.findFirst().get()
-        topics = topics.minus(topic).plus(Topic(
+        val topicUpdated = Topic(
             id = form.id,
             title = form.title,
             message = form.message,
@@ -42,7 +43,9 @@ class TopicService(
             responses = topic.responses,
             status = topic.status,
             creationDate = topic.creationDate
-        ))
+        )
+        topics = topics.minus(topic).plus(topicUpdated)
+        return topicDtoMapper.map(topicUpdated)
     }
 
     fun delete(id: Long) {
