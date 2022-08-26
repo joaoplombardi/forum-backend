58565f4231f469dfb7aa513e9d3bc31274075bd7
Joao <joaopedrolombardiv@gmail.com>
2022-08-26 20:53:44 -0300

    Feat(models): Aplicando as anotações para declarar a entidade

--- src/main/kotlin/br/com/projects/forum/forum/model/Resposta.kt
+++ src/main/kotlin/br/com/projects/forum/forum/model/Resposta.kt
@@ -1,12 +1,22 @@
 package br.com.projects.forum.forum.model
 
 import java.time.LocalDateTime
+import javax.persistence.Entity
+import javax.persistence.GeneratedValue
+import javax.persistence.GenerationType
+import javax.persistence.Id
+import javax.persistence.ManyToOne
 
+@Entity
 data class Resposta(
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     val id: Long? = null,
     val message: String,
     val criationDate: LocalDateTime = LocalDateTime.now(),
+    @ManyToOne
     val author: User,
+    @ManyToOne
     val topic: Topic,
     val solution: Boolean
 )
--- src/main/kotlin/br/com/projects/forum/forum/model/Topic.kt
+++ src/main/kotlin/br/com/projects/forum/forum/model/Topic.kt
@@ -1,14 +1,22 @@
 package br.com.projects.forum.forum.model
 
 import java.time.LocalDateTime
+import javax.persistence.*
 
+@Entity
 data class Topic (
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     var id: Long? = null,
-    val title: String,
-    val message: String,
+    var title: String,
+    var message: String,
     val creationDate: LocalDateTime = LocalDateTime.now(),
+    @ManyToOne
     val course: Course,
+    @ManyToOne
     val author: User,
+    @Enumerated(value = EnumType.STRING)
     val status: TopicStatus = TopicStatus.NOT_ANSWERED,
+    @OneToMany(mappedBy = "topic")
     val responses: List<Resposta> = ArrayList()
 )
